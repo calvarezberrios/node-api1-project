34d7a8dccc94cc2ceb0c96c275e32ed515afc3e0
Carlos 'Mannie' Alvarez <carlos.alvarezberrio@gmail.com>
2020-07-08 00:36:18 -0400

    cleaned up and added comment labels for CRUD sections

--- server.js
+++ server.js
@@ -20,6 +20,36 @@ server.get("/", (req, res) => {
     res.status(200).send("Welcome to my very first API!");
 });
 
+// USERS CRUD
+
+// CREATE
+server.post("/api/users", (req, res) => {
+    const newUser = req.body;
+
+    if(!(newUser.name || newUser.bio)) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    }
+
+    try {
+        const itExists = users.find(user => user.name === req.body.name);
+
+        if(!itExists) {
+            newUser.id = shortid.generate();
+            users.push(newUser);
+            res.status(201).json(newUser);
+        } else {
+            res.status(400).json({ errorMessage: "A user with that name already exists!" });
+        }
+        
+    }
+    catch {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+    }
+});
+
+
+
+// READ
 server.get("/api/users", (req, res) => {
     try {
         res.status(200).json(users);
@@ -47,31 +77,36 @@ server.get("/api/users/:id", (req, res) => {
 
 });
 
-server.post("/api/users", (req, res) => {
-    const newUser = req.body;
 
-    if(!(newUser.name || newUser.bio)) {
+
+// UPDATE
+server.put("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    changes.id = id;
+
+    if(!(changes.name || changes.bio)) {
         res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
     }
 
-    try {
-        const itExists = users.find(user => user.name === req.body.name);
+    try {        
+        const index = users.findIndex(user => user.id === id);
 
-        if(!itExists) {
-            newUser.id = shortid.generate();
-            users.push(newUser);
-            res.status(201).json(newUser);
+        if(index !== -1) {            
+            users[index] = changes;
+            res.status(200).json(changes);
         } else {
-            res.status(400).json({ errorMessage: "A user with that name already exists!" });
+            res.status(404).json({ errorMessage: "The user with the specified ID does not exist." })
         }
-        
-    }
+    }   
     catch {
-        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+        res.status(500).json({ errorMessage: "The user information could not be modified." });
     }
 });
 
 
+
+// DELETE/DESTROY
 server.delete("/api/users/:id", (req, res) => {
     const id = req.params.id;
     const found = users.find(user => user.id === id);
@@ -86,30 +121,6 @@ server.delete("/api/users/:id", (req, res) => {
     }   
     catch {
         res.status(500).json({ errorMessage: "The user could not be removed" });
-    }
-});
-
-server.put("/api/users/:id", (req, res) => {
-    const id = req.params.id;
-    const changes = req.body;
-    changes.id = id;
-
-    if(!(changes.name || changes.bio)) {
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
-    }
-
-    try {        
-        const index = users.findIndex(user => user.id === id);
-
-        if(id !== -1) {            
-            users[index] = changes;
-            res.status(200).json(changes);
-        } else {
-            res.status(404).json({ errorMessage: "The user with the specified ID does not exist." })
-        }
-    }   
-    catch {
-        res.status(500).json({ errorMessage: "The user information could not be modified." });
     }
 });
 
